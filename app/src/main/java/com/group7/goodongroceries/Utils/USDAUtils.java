ed72a7a4a4119f03063faa466ac9e55efba8ea13
codymalick <cody.malick@gmail.com>
2017-06-02 13:58:03 -0700

    Completed two needed api url builders, added basic tests for both functions

--- app/src/main/java/com/group7/goodongroceries/Utils/USDAUtils.java
+++ app/src/main/java/com/group7/goodongroceries/Utils/USDAUtils.java
@@ -2,12 +2,14 @@ package com.group7.goodongroceries.Utils;
 
 import android.net.Uri;
 
+import com.group7.goodongroceries.Models.Nutrient;
+
 import java.io.Serializable;
 
 public class USDAUtils {
 
-    private final static String USDA_REPORT_BASE_URL = "https://api.nal.usda.gov/ndb/V2/reports";
     private final static String USDA_SEARCH_BASE_URL = "https://api.nal.usda.gov/ndb/search/";
+    private final static String USDA_NUTRIENT_BASE_URL = "https://api.nal.usda.gov/ndb/nutrients/";
 
     private final static String USDA_QUERY_PARAM = "q";
 
@@ -21,7 +23,15 @@ public class USDAUtils {
     private final static String USDA_MAX_VAL = "20"; // Total items returned
 
     private final static String USDA_API_KEY_PARAM = "api_key";
-    private final static String USDA_API_KEY = "4074a8f5-dc8a-4b5b-bf38-fc5d8f500667";
+    public final static String USDA_API_KEY = "AiC0tHOnH56DABVtkqKpXkHD6T4Px54virs0mvW2";
+
+    private final static String USDA_NUTRIENTS_PARAM = "nutrients";
+    private final static String USDA_NDBNO_PARAM = "ndbno";
+
+    private static String sugar_ndbo = "269";
+    private static String carbs_ndbo = "205";
+    private static String calories_ndbo = "208";
+    private static String lipidfat_ndbo = "204";
 
     public static class FoodItem implements Serializable {
 
@@ -35,6 +45,20 @@ public class USDAUtils {
                 .appendQueryParameter(USDA_QUERY_PARAM, food)
                 .appendQueryParameter(USDA_SORT_PARAM, USDA_SORT_VAL)
                 .appendQueryParameter(USDA_MAX_PARAM, USDA_MAX_VAL)
+                .appendQueryParameter(USDA_API_KEY_PARAM, USDA_API_KEY)
+                .build()
+                .toString();
+    }
+
+    // Search for specific food using ndbno
+    public static String buildNutrientQueryURL(String ndbno) {
+        return Uri.parse(USDA_NUTRIENT_BASE_URL).buildUpon()
+                .appendQueryParameter(USDA_FORMAT_PARAM, USDA_FORMAT_VAL)
+                .appendQueryParameter(USDA_NDBNO_PARAM, ndbno)
+                .appendQueryParameter(USDA_NUTRIENTS_PARAM, sugar_ndbo)
+                .appendQueryParameter(USDA_NUTRIENTS_PARAM, calories_ndbo)
+                .appendQueryParameter(USDA_NUTRIENTS_PARAM, carbs_ndbo)
+                .appendQueryParameter(USDA_NUTRIENTS_PARAM, lipidfat_ndbo)
                 .appendQueryParameter(USDA_API_KEY_PARAM, USDA_API_KEY)
                 .build()
                 .toString();
