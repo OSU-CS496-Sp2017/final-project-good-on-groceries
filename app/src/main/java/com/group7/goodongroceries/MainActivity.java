8d884b0a0b71b04b53e889e75deb8746b0d2b833
Dan Stoyer <stoyerd@oregonstate.edu>
2017-06-08 20:28:51 -0700

    Changed the action bar color

--- app/src/main/java/com/group7/goodongroceries/MainActivity.java
+++ app/src/main/java/com/group7/goodongroceries/MainActivity.java
@@ -171,7 +171,7 @@ public class MainActivity extends AppCompatActivity
                 }
                 builder.setTitle("Delete Selected Items")
                 .setMessage("Are you sure?")
-                .setIcon(R.drawable.ic_action_delete_selected_red)
+                .setIcon(R.drawable.ic_action_delete_selected_white)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -193,7 +193,7 @@ public class MainActivity extends AppCompatActivity
                 }
                 builder.setTitle("Delete All Items")
                 .setMessage("Are you sure?")
-                .setIcon(R.drawable.ic_action_delete_all_red)
+                .setIcon(R.drawable.ic_action_delete_all_white)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
